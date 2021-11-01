647a278a95103dc038a5306cab3f94b5e280ca56
Roberto <ruidaing@hotmail.com>
2021-11-01 07:41:07 -0500

    [msi/river] create architecture project

---
+++ src/Pantallas/Tienda/AddProducto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function AddProducto(){
+    return(
+        <View>
+            <Text>AddProducto</Text>
+        </View>
+    );
+}
