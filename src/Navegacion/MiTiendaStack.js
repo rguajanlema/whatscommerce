791b0f4d7d77c8c09412be9527c785fc9e7ecae9
Roberto <ruidaing@hotmail.com>
2021-11-01 09:01:22 -0500

    [msi/river] funcionalidad de tiendastack

---
+++ src/Navegacion/MiTiendaStack.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/native';
+
+import MiTienda from '../Pantallas/MiTienda/MiTienda';
+import EditarProducto from '../Pantallas/MiTienda/EditarProducto';
+
+const Stack = createStackNavigator();
+
+export default function MiTiendaStack()
+{
+    return(
+        <Stack.Navigator
+        screenOptions={{
+            headerStyle:{ backgroundColor:"#128C7E", headerTintColor: "#fff"},
+        }}
+        >
+            <Stack.Screen 
+            component={MiTienda}
+            name="mitienda"
+            options={{
+                title: "Mi Tienda"
+            }}
+            />
+            <Stack.Screen 
+            component={EditarProducto}
+            name="edit-product"
+            options={{
+                title: "Editar Producto"
+            }}
+            />
+        </Stack.Navigator>
+    );
+}
