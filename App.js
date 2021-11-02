6ad694bb3d2d01531f48bdc1dae402c47efd2a82
Roberto <ruidaing@hotmail.com>
2021-11-01 21:07:02 -0500

    [msi/river] creacion del componente Login

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import RutasAutenticadas from './src/Navegacion/RutasAutenticadas';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RutasAutenticadas from "./src/Navegacion/RutasAutenticadas";
+import RutasNoAutenticadas from "./src/Navegacion/RutasNoAutenticadas";
 
 export default function App() {
-  return <RutasAutenticadas/>;
+  return <RutasNoAutenticadas/>;
 }
 
 const styles = StyleSheet.create({
