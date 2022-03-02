d11b67941a77913ce695c1b917670a734c3f9073
Roberto <ruidaing@hotmail.com>
2022-03-01 19:50:55 -0500

    update form registerform whit more features

--- src/Componentes/RegisterForm.js
+++ src/Componentes/RegisterForm.js
@@ -1,14 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Icon, Input, Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { validaremail } from "../Utils/Utils";
+import { isEmpty } from "lodash";
+//import * as firebase from "firebase";
 
-export default function RegisterForm(){
-    return(
-        <View>
-            <Text>Register form</Text>
+export default function RegisterForm(props) {
+    const { toastRef } = props;
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [repetirpassword, setrepetirpassword] = useState("");
+    const [show, setshow] = useState(true);
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <View
+                style={{
+                    borderBottomColor: "#25D366",
+                    borderBottomWidth: 2,
+                    width: 100,
+                }}
+            />
+            <Input placeholder="Correo"
+                containerStyle={styles.input}
+                rightIcon={{
+                    type: "material-community",
+                    name: "at",
+                    color: "#128c7e",
+                }}
+                leftIcon={{
+                    type: "material-community",
+                    name: "account-circle-outline",
+                    color: "#128c7e"
+                }}
+                onChangeText={(text) => {
+                    setemail(text);
+                }}
+                value={email}
+            />
+            <Input placeholder="Contraseña"
+                containerStyle={styles.input}
+                leftIcon={{
+                    type: "material-community",
+                    name: "security",
+                    color: "#128c7e"
+                }}
+                rightIcon={{
+                    type: "material-community",
+                    name: show ? "eye-outline" : "eye-off-outline",
+                    color: "#128c7e",
+                    onPress: () => setshow(!show)
+                }}
+                onChangeText={(text) => {
+                    setpassword(text);
+                }}
+                secureTextEntry={show}
+                value={password}
+            />
+            <Input placeholder="Repetir Contraseña"
+                containerStyle={styles.input}
+                leftIcon={{
+                    type: "material-community",
+                    name: "security",
+                    color: "#128c7e"
+                }}
+                rightIcon={{
+                    type: "material-community",
+                    name: show ? "eye-outline" : "eye-off-outline",
+                    color: "#128c7e",
+                    onPress: () => setshow(!show)
+                }}
+                onChangeText={(text) => {
+                    setrepetirpassword(text);
+                }}
+                secureTextEntry={show}
+                value={repetirpassword}
+            />
+            <Button title="CREAR CUENTA"
+                containerStyle={styles.btnentrar}
+                buttonStyle={{ backgroundColor: "#25d366" }}
+                onPress={() => console.log("Crear cuenta")}
+            />
+            <Button title="INICIAR SESION"
+                containerStyle={styles.btnentrar}
+                buttonStyle={{ backgroundColor: "#128C7E" }}
+                onPress={() => navigation.goBack()}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#F5F6F8",
+        flex: 1,
+        marginTop: 20,
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        alignItems: "center",
+        paddingTop: 20
+    },
+    input: {
+        width: "90%",
+        marginTop: 20,
+        height: 50,
+    },
+    btnentrar: {
+        width: "90%",
+        marginTop: 20,
+    },
 
 });
--- src/Pantallas/Cuenta/Registrar.js
+++ src/Pantallas/Cuenta/Registrar.js
@@ -7,15 +7,15 @@ import RegisterForm from '../../Componentes/RegisterForm';
 export default function Registrar(){
 
     const toastRef = useRef();
-    
+
     return(
-        <View>
+        <View style={styles.container}>
             <StatusBar backgroundColor='#128C73'/>
             <Image
             source = {require("../../../assets/logo.png")}
             style={styles.imglogo}
             />
-            <Text style={styles.textobaner}>!Bienvenido</Text>
+            <Text style={styles.textobaner}>CREAR CUENTA</Text>
             <RegisterForm toastRef = {toastRef}/>
             <Toast ref={toastRef} position="center" opacity={0.9}/>
         </View>
