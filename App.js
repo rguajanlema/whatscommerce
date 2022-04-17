77d7514b9711e84acf66f8be61d07662862ffb49
Roberto <ruidaing@hotmail.com>
2022-04-17 07:27:25 -0500

    54. 24 - FUNCION DE ENVIAR CONFIRMACION-

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
-import { StatusBar } from "expo-status-bar";
+//import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { YellowBox } from "react-native-web";
+//import { YellowBox } from "react-native-web";
 import Loading from "./src/Componentes/Loading";
 import RutasNoAutenticadas from "./src/Navegacion/RutasNoAutenticadas";
 import SwitchNavigator from "./src/Navegacion/SwitchNavigator";
 import { validarsesion } from "./src/Utils/Acciones";
 
-YellowBox.ignoreWarnings(["Animated"]);
+//YellowBox.ignoreWarnings(["Animated"]);
 
 export default function App() {
-  const [user, setuser] = useState(false);
+  const [user, setUser] = useState(false);
   const [loading, setloading] = useState(false);
 
   useEffect(() => {
     setloading(true);
-    validarsesion(setuser);
+    validarsesion(setUser);
     setloading(false);
   }, []);
   if (loading) {
     return <Loading isVisible={loading} text="Cargando.." />;
   }
-
-  return user ?<SwitchNavigator/> : <RutasNoAutenticadas />;
+  return user ? <SwitchNavigator /> : <RutasNoAutenticadas />;
 }
 
 const styles = StyleSheet.create({
--- src/Utils/Acciones.js
+++ src/Utils/Acciones.js
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react";
 import { firebaseApp } from "./Firebase";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const auth = getAuth();
 
-export const validarsesion =(setvalidarsesion)=>{
-    onAuthStateChanged(auth,(user)=>{
-        if(user){
-            setvalidarsesion(true);
-        }else{
-            setvalidarsesion(false);
-        }
-    });
+export const validarsesion = (setvalidarsesion) => {
+  console.log("setvalidarsesion");
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setvalidarsesion(true);
+    } else {
+      setvalidarsesion(false);
+    }
+  });
 };
 
 export const cerrarsesion = () => {
-    signOut(auth);
-}
+  signOut(auth);
+};
 
 export const validadPhone = (setphoneauth) => {
-    onAuthStateChanged(auth, (user)=>{
-        if(user.phoneNumber){
-            setphoneauth(true);
-        }
-    });
+  onAuthStateChanged(auth, (user) => {
+    if (user.phoneNumber) {
+      setphoneauth(true);
+    }
+  });
 };
